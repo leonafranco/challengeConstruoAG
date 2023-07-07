80efd3722a0a7139f7350ba6c635b41f6933948d
Leonardo Franco <leonardofranco@Leonardos-MBP-2.home>
2023-07-07 09:01:25 +0100

    1st commit, empty spring initializer app

---
+++ src/main/java/com/challenge/ChallengeConstruoAG/ChallengeConstruoAgApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.ChallengeConstruoAG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeConstruoAgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeConstruoAgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/ChallengeConstruoAG/ChallengeConstruoAgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.ChallengeConstruoAG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeConstruoAgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
